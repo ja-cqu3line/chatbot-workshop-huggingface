d2cc26b75b4290c706787093cd3cdd08cf9a9495
ja-cqu3line <coxjackie55@gmail.com>
2026-02-11 13:24:56 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,14 +14,14 @@ def get_stopwords():
     nltk.download('stopwords')
 
 st.set_page_config(page_title="Chat with a friend on the works of Rabindranath Tagore", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
-st.title("Chat with a friend on the works of Rabindranath Tagore")
+st.title("Chat about courses at San Jose State University")
 
 
 if "messages" not in st.session_state.keys():  # Initialize the chat messages history
     st.session_state.messages = [
         {
             "role": "assistant",
-            "content": "Ask me a question about Rabindranath Tagore!!",
+            "content": "Ask me a question about Intro to Engineering and General Chemistry!!",
         }
     ]
 
@@ -44,10 +44,10 @@ def load_data():
     token=st.secrets.hftoken,
     generate_kwargs={"temperature": 0.7, "top_k": 50, "top_p": 0.95},
     provider="auto",  # this will use the best provider available
-    system_prompt="""You are an expert on the work of Rabindranath Tagore.
-    Answer the question using the provided documents, which contain relevant excerpts from the work of Rabindranath Tagore.
-    The context for all questions is the work of Rabindranath Tagore. Whenever possible, include a quotation from the provided excerpts of his work to illustrate your point.
-    Respond using a florid but direct tone, typical of an early modernist writer.
+    system_prompt="""You are an expert on the General Chemistry and Intro to Engineering courses at San Jose State University.
+    Answer the question using the provided documents, which contain information on the courses.
+    The context for all questions is the General Chemisty and Intro to Engineering courses at San Jose State University. Whenever possible, include a quotation from the provided documents to ensure reliability.
+    Respond using a positive but direct tone, typical of a university professor.
     Respond in fewer than 100 words.""",
     )
     index = VectorStoreIndex.from_documents(docs)
